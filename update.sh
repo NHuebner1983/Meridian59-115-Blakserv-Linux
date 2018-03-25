99e8c7df36a324b9fea908e3a270de2f8347d619
Nathan Huebner <ndallas1983@gmail.com>
2018-03-24 20:46:27 -0500

    Hold double weight. Phase fix (now allowed in PVP).

--- update.sh
+++ update.sh
@@ -1,3 +1,11 @@
+echo "Performing local operations first"
+
+echo "Copying BOF files to loadkod..."
+find /mnt/d/Meridian59/kod/ -name '*.bof' -print0 | xargs -0 cp -t /mnt/d/Meridian59/run/server/loadkod/
+
+echo "Copying RSC files to rsc..."
+find /mnt/d/Meridian59/kod/ -name '*.rsc' -print0 | xargs -0 cp -t /mnt/d/Meridian59/run/server/rsc/
+
 echo "Synchronizing loadkod on public game server..."
 sshpass -f /secure-server.us.pass rsync --size-only /mnt/d/Meridian59/run/server/loadkod/* root@secure-server.us:/meridian/run/server/loadkod/
 
